124ae0aeb88c0510bad6a0c552e680fc7b65407c
Erika Fernández Moreno <erika@edrakon.tech>
2024-02-26 12:03:22 +0100

    Mejoras visuales en la representación del mapa

--- src/arrays/BattleShips.java
+++ src/arrays/BattleShips.java
@@ -44,7 +44,7 @@ public class BattleShips {
 
     public static void createOceanMap(){
         //First section of Ocean Map
-        System.out.print("  ");
+        System.out.print("   ");
         for(int i = 0; i < numCols; i++)
             System.out.print(i + " ");
         System.out.println();
@@ -54,11 +54,11 @@ public class BattleShips {
             for (int j = 0; j < grid[i].length; j++) {
                 grid[i][j] = " ";
                 if (j == 0)
-                    System.out.print(i + "| " + grid[i][j]);
+                    System.out.print(i + "| " + grid[i][j] + " ");
                 else if (j == grid[i].length - 1)
                     System.out.print(grid[i][j] + " |" + i);
                 else
-                    System.out.print(grid[i][j]);
+                    System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
@@ -201,26 +201,26 @@ public class BattleShips {
     public static void printOceanMap(){
         System.out.println();
         //First section of Ocean Map
-        System.out.print("  ");
+        System.out.print("   ");
         for(int i = 0; i < numCols; i++)
             System.out.print(i + " ");
         System.out.println();
 
         //Middle section of Ocean Map
         for(int x = 0; x < grid.length; x++) {
-            System.out.print(x + "|");
+            System.out.print(x + "| ");
 
             for (int y = 0; y < grid[x].length; y++){
-                System.out.print(grid[x][y]);
+                System.out.print(grid[x][y] + " ");
             }
 
-            System.out.println("|" + x);
+            System.out.println(" |" + x);
         }
 
         //Last section of Ocean Map
-        System.out.print("  ");
+        System.out.print("   ");
         for(int i = 0; i < numCols; i++)
-            System.out.print(i);
+            System.out.print(i + " ");
         System.out.println();
     }
 }
