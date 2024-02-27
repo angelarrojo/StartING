e868b2477f24884d1b7cb298c052321f99756a9f
Paulo Franklim dos Santos de Carvalho <paulo.franklim.dos.santos.de.carvalho@ing.com>
2024-02-27 11:58:42 +0100

    String hands on exercise

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # StartING
 Introducción al lenguaje Java
+
+### Installation
+Clone the repo
+   ```sh
+   git clone https://github.com/github_username/repo_name.git
+   ```
---
+++ src/strings/StringHandsOn.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class StringHandsOn {
+
+    public static void main(String[] args) {
+
+        /**
+         *
+         * 1- Leer dos Strings por la consola
+         * 2- Imprimir la longitud de la primera palabra
+         * 3- Imprimir la segunda palabra toda en mayuscula
+         * 4- Imprimir el primer caracter de la primera palabra, ultimo caracter de la segunda y la suma de la longitud de las dos palabras en una unica linea.
+         *
+         * Ayuda:
+         * Scanner scanner = new Scanner(System.in); //CREA EL OBJETO SCANNER PARA PODER LEER DE LA CONSOLA
+         * scanner.nextLine(); // RECIBE EL VALOR DE LA CONSOLA
+         * System.out.println("Texto"); // METODO PARA PINTAR EN LA CONSOLA
+         *
+         * */
+
+    }
+}
