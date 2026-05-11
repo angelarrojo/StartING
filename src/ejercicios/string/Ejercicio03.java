a4618e78664ec0507069e6b9546071915c54c3a5
AO49UX <yuliang.wang@ing.com>
2026-05-11 09:18:42 +0200

    string 0511

---
+++ src/ejercicios/string/Ejercicio03.java
@@ -0,0 +1,12 @@
+package ejercicios.string;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+
+        String texto = "Java";
+
+        // TODO:
+        // Muestra por pantalla la longitud del String
+
+    }
+}
---
+++ src/ejercicios/string/Ejercicio11.java
@@ -0,0 +1,38 @@
+package ejercicios.string;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+
+        String mensaje = "CAMBIA_ESTE_MENSAJE";
+
+        int espacio = mensaje.indexOf(" ");
+        int guion = mensaje.indexOf("-");
+
+        String resultado = mensaje.substring(0, espacio).toUpperCase() + "_" + mensaje.substring(guion + 1).toLowerCase();
+        System.out.println(resultado);
+
+        /*
+         EJERCICIO:
+
+         NO se puede modificar el código.
+         SOLO se puede cambiar el contenido de la variable mensaje.
+
+         OBJETIVO:
+         Consigue que la salida sea EXACTAMENTE:
+
+         HOLA_mundo
+
+         PISTAS:
+         - El programa usa la posición del PRIMER espacio
+         - El programa usa la posición del PRIMER guion '-'
+         - Parte del mensaje se pierde
+         - Parte del mensaje se transforma a mayúsculas
+         - Parte del mensaje se transforma a minúsculas
+
+         RESTRICCIONES:
+         - NO se puede tocar el código
+         - NO se pueden escribir los resultados a mano
+         - SOLO se modifica el valor de mensaje
+        */
+    }
+}
